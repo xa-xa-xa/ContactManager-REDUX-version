3d3b8e391437a8c1874142b02b1e41ba8ba06e04
xa-xa-xa <28684394+xa-xa-xa@users.noreply.github.com>
2018-09-04 17:44:34 -0700

    "added contactActions and got riddoff  maps..."

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Contact from './Contact';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { GET_CONTACTS } from '../../actions/types';
+import { getContacts } from '../../actions/contactActions';
 
 class Contacts extends Component {
   componentDidMount() {
@@ -34,14 +34,7 @@ const mapStateToProps = state => ({
   contacts: state.contact.contacts
 });
 
-const mapDispatchToProps = dispatch => ({
-  getContacts: () =>
-    dispatch({
-      type: GET_CONTACTS
-    })
-});
-
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  { getContacts }
 )(Contacts);
