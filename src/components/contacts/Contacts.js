4f47556789c8ee162845b45057969befdb802150
xa-xa-xa <28684394+xa-xa-xa@users.noreply.github.com>
2018-09-04 17:37:09 -0700

    "moved contacts in contactReducers from Contacts"

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,47 +1,16 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { GET_CONTACTS } from '../../actions/types';
 
 class Contacts extends Component {
-  // constructor() {  // ---this is optional if we need itialisation
-  //   super();       //
-  //   this.state = { //
-  state = {
-    contacts: [
-      {
-        id: '1',
-        name: 'Jond Doe',
-        email: 'jdoe@email.com',
-        phone: '111-111-1111'
-      },
-      {
-        id: '2',
-        name: 'Peter Smith',
-        email: 'psmith@email.com',
-        phone: '222-222-2222'
-      },
-      {
-        id: '3',
-        name: 'Marta Johnes',
-        email: 'mjohnes@email.com',
-        phone: '333-333-3333'
-      },
-      {
-        id: '4',
-        name: 'Will Marks',
-        email: 'wmarks@email.com',
-        phone: '444-444-4444'
-      },
-      {
-        id: '5',
-        name: 'Glen Britch',
-        email: 'gbritch@email.com',
-        phone: '555-555-5555'
-      }
-    ]
-  };
+  componentDidMount() {
+    this.props.getContacts();
+  }
 
   render() {
-    const { contacts } = this.state;
+    const { contacts } = this.props;
     return (
       <React.Fragment>
         <h1 className="dispay-4 mb-2">
@@ -56,4 +25,23 @@ class Contacts extends Component {
   }
 }
 
-export default Contacts;
+Contacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  getContacts: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  contacts: state.contact.contacts
+});
+
+const mapDispatchToProps = dispatch => ({
+  getContacts: () =>
+    dispatch({
+      type: GET_CONTACTS
+    })
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Contacts);
--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,9 +1,46 @@
+import { GET_CONTACTS } from '../actions/types';
+
 const initialState = {
-  //   test: 'test'
+  contacts: [
+    {
+      id: '1',
+      name: 'Jond Doe',
+      email: 'jdoe@email.com',
+      phone: '111-111-1111'
+    },
+    {
+      id: '2',
+      name: 'Peter Smith',
+      email: 'psmith@email.com',
+      phone: '222-222-2222'
+    },
+    {
+      id: '3',
+      name: 'Marta Johnes',
+      email: 'mjohnes@email.com',
+      phone: '333-333-3333'
+    },
+    {
+      id: '4',
+      name: 'Will Marks',
+      email: 'wmarks@email.com',
+      phone: '444-444-4444'
+    },
+    {
+      id: '5',
+      name: 'Glen Britch',
+      email: 'gbritch@email.com',
+      phone: '555-555-5555'
+    }
+  ]
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case GET_CONTACTS:
+      return {
+        ...state
+      };
     default:
       return state;
   }
